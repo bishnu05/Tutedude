ad6910adb8c527d6d355588026f914501bb86209
bishnu05 <101577876+bishnu05@users.noreply.github.com>
2026-08-31 00:27:13 +0530

    Task 13 done

---
+++ bishnu_task13/script.js
@@ -0,0 +1,14 @@
+let num = 5;
+let num2 = 10;
+
+if (num % 2 === 0) {
+  console.log("The Number " + num + " is even");
+} else {
+  console.log("The Number " + num + " is odd");
+}
+
+if (num2 % 2 === 0) {
+  console.log("The Number " + num2 + " is even");
+} else {
+  console.log("The Number " + num2 + " is odd");
+}
---
+++ bishnu_task7/Readme.md
@@ -0,0 +1,1 @@
+CSS - Laundry Services Hero Section
